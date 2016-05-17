2d72c1371379f9952b0162e9dec1fac5454314c7
Jonathan Dunlap <jdunlap@outlook.com>
2016-05-17 11:30:12 -0700

    wip - obj perf

--- test/perf/filter-map-reduce.js
+++ test/perf/filter-map-reduce.js
@@ -12,10 +12,10 @@ var kefirFromArray = runners.kefirFromArray;
 
 // Create a stream from an Array of n integers
 // filter out odds, map remaining evens by adding 1, then reduce by summing
-var n = runners.getIntArg(10000);
+var n = runners.getIntArg(24*7);
 var a = new Array(n);
 for (var i = 0; i < a.length; ++i) {
-    a[i] = i;
+    a[i] = { data: i, other: i % 2 === 0 };
 }
 
 var suite = Benchmark.Suite('filter -> map -> reduce ' + n + ' integers');
@@ -31,7 +31,7 @@ suite
         runners.runArray(deferred, runArray2);
     }, options)
     .add('most', function(deferred) {
-        runners.runMost(deferred, most.from(a).filter(even).map(add1).reduce(sum, 0));
+        runners.runMost(deferred, most.from(a).filter(even).map(add1).reduce(sum, {}));
     }, options)
     //.add('rx 4', function(deferred) {
     //    runners.runRx(deferred, rx.Observable.fromArray(a).filter(even).map(add1).reduce(sum, 0));
@@ -59,22 +59,46 @@ suite
 runners.runSuite(suite);
 
 function add1(x) {
-    return x + 1;
+    x.data += 1;
+    return x;
 }
 
 function even(x) {
+    return x.data % 2 === 0;
+}
+
+function sum(acc, y) {
+   acc.data =+ y.data;
+   if (!acc.others) acc.others = [];
+   acc.others.push(y.other);
+   return acc;
+}
+
+function add12(x) {
+    return x + 1;
+}
+
+function even2(x) {
     return x % 2 === 0;
 }
 
-function sum(x, y) {
+function sum2(x, y) {
     return x + y;
 }
 
 function runLodash(cb) {
-    return lodash.chain(a).filter(even).map(add1).reduce(sum, 0).value();
+    return lodash.chain(a).filter(even).map(add1).reduce(sum, {}).value();
 }
 
 function runArray(cb) {
+    return a.filter(even).map(add1).reduce(sum, {});
+}
+
+function runLodash3(cb) {
+    return lodash.chain(a).filter(even).map(add1).reduce(sum, 0).value();
+}
+
+function runArray3(cb) {
     return a.filter(even).map(add1).reduce(sum, 0);
 }
 
@@ -84,15 +108,17 @@ function runArray2() {
 		if(even(a[i])===true) x1.push(a[i]);
 	}
 
-	let x2 = []; //new Array(x1.length);
+	let x2 = x1;//new Array(x1.length); //new Array(x1.length);
 	for(var i=0, n=x1.length; i < n; i++) {
-		x2[i] = add1(x1[i]);
+        const v = x1[i];
+		x2[i] = add1(v);
 	}
 
 
 	let reduce = 0;
 	for(var i=0, n=x1.length; i < n; i++) {
-		reduce = sum(reduce, x2[i])
+        const v = x2[i];
+		reduce = sum(reduce, v);
 	}
 
 	return reduce;
